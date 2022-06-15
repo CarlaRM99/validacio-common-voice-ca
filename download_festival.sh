f60bdea9fde22cca2af8867adb84874bd23ba02b
CarlaRM99 <carla.ramirez.marti@gmail.com>
2022-06-15 16:35:06 +0200

    Added and edited files

---
+++ download_festival.sh
@@ -0,0 +1,15 @@
+
+# Links to download FESTIVAL 2.4
+wget http://www.cstr.ed.ac.uk/downloads/festival/2.4/festival-2.4-release.tar.gz
+wget http://www.cstr.ed.ac.uk/downloads/festival/2.4/festlex_CMU.tar.gz
+wget http://www.cstr.ed.ac.uk/downloads/festival/2.4/festlex_OALD.tar.gz
+wget http://www.cstr.ed.ac.uk/downloads/festival/2.4/festlex_POSLEX.tar.gz
+wget http://www.cstr.ed.ac.uk/downloads/festival/2.4/speech_tools-2.4-release.tar.gz
+
+wget http://www.cstr.ed.ac.uk/downloads/festival/2.4/voices/festvox_cmu_us_awb_cg.tar.gz
+wget http://www.cstr.ed.ac.uk/downloads/festival/2.4/voices/festvox_cmu_us_slt_cg.tar.gz
+wget http://www.cstr.ed.ac.uk/downloads/festival/2.4/voices/festvox_kallpc16k.tar.gz
+wget http://www.cstr.ed.ac.uk/downloads/festival/2.4/voices/festvox_rablpc16k.tar.gz
+
+wget http://festvox.org/festvox-2.7/festvox-2.7.0-release.tar.gz
+
